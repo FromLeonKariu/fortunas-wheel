969d97794c410e192980734796126ebccaad274a
Fromleonkariu <leonkariu@outlook.com>
2024-07-26 01:00:17 +0300

    placed media query at the end of the css for readability and to solve bugs

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 console.log("We are live, Fortuna's wheel");
 
-window.alert("Welcome to the webiste... The Js is successfully connected");
+function triangle() {
+    var star = "*";
+    var stars = "*";
+    for (var i = 0; i < 5; i++) {
+        console.log(stars);
+        stars = stars + star;
+    }
+}
+
+triangle();
